f4203f5e82eaa89598df6e9c9bf7bd41d47c5ac0
pipinosuke <ys.pipinosuke@gmail.com>
2021-11-03 19:40:12 +0900

    edit site config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Blog-add46ffe71fc44c08b979f27ccb91f72',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Yusuke Sugino',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Yusuke Sugino',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: '思考と行動の軌跡を残しています',
+  socialImageTitle: 'どっかのエンジニアのブログ',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '#',
+  github: '#',
+  linkedin: '#',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
